125a32eebc1c19309c1dc89000c161b0de3b16fc
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2021-04-05 15:15:24 +0200

    Logitech tracking setup and working

--- WebParser/website.py
+++ WebParser/website.py
@@ -25,15 +25,32 @@ class website():
         self.isChanged = False
 
     def update(self):
+        """
+
+        Notes:
+        - Solution might be unstable. If black is no longer available, the algorithm
+        might assume that white is. This is due to logitechs strange web app which
+        automatically switches the selected color.
+        - Currently, no error handling available. If logitech website breaks, the whole
+          script crashes
+        - WebDriverWait doesnt seem to do a thing
+        - implicit wait is a cleaner solution than sleep, but tutorials only show how
+          to wait for an element with given id. Solution could be something like
+          "WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,
+          '//*[@id="onetrust-accept-btn-handler"]')))" but it doesnt seem to work
+          out.
+        """
         driver = self.getWebDriver()
+        # waits until website is available, max 10 seconds
         driver.implicitly_wait(10)
-        sleep(5)
         # -------- Accept all cookies
+        # cookies might bob up after the page is fully loaded already
+        sleep(5)
         driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]').click()
-        sleep(4)
         # -------- select switches and keyboard layout
         # find_element_by_class_name returns only the first element of find_elements_by_class_name
         # unfortunately, all dropdown menus share the same class name.
+        # sleep(5)
         clickSelection = driver.find_elements_by_class_name("js-product-model-selector")
         languageSelection = clickSelection[0]
         languageDropdownMenu = Select(languageSelection)
@@ -50,7 +67,6 @@ class website():
         picture = driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div[12]/section/div/div/div[1]/div[17]/div[1]/div/ul/div/div/li[1]/div')
         colorsWhite = driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div[12]/section/div/div/div[2]/div/div[4]/div[1]/ul/li[2]/button').is_enabled()
         colorBlack = driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div[12]/section/div/div/div[2]/div/div[4]/div[1]/ul/li[1]/button').is_enabled()
-        sleep(4)
         outerHtml = driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div[12]/section/div/div/div[1]/div[12]/div[1]/div/ul/div/div/li[1]/div/img').get_attribute('outerHTML')
         if re.search(r'tkl-carbon-gallery' ,outerHtml):
             self.isChanged = False
